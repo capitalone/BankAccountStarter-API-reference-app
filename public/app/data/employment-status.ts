6e9f6548f289f18b760dfb33a46ad5e7519164b7
Giacometti, Brian <diu779@MUSADE207599.cof.ds.capitalone.com>
2017-02-15 14:33:28 -0500

    Updated code in prospect form component and added data to data-constants.ts

--- public/app/data/employment-status.ts
+++
@@ -1,7 +0,0 @@
-export const employment: Array<any> = [
-        {name: 'Employed'},
-        {name: 'Self-Employed'},
-        {name: 'Retired'},
-        {name: 'Student'},
-        {name: 'Unemployed'},
-];
