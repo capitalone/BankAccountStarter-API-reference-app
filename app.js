b47209c78dcbac2a7899633d9aa9f6cd538088fa
KeithTeesdale <keith.teesdale@baruchmail.cuny.edu>
2017-08-16 15:21:37 -0400

    revert app.js

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 /**
 Copyright 2016 Capital One Services, LLC
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,16 +28,7 @@ var CreateAccountClient = require('./api/CreateAccountClient');
 var CDTermsClient = require('./api/CDTermsClient');
 var oauth = require('./api/oauth');
 var oauthOptions = {
-      //tokenURL: config.BASE_URI + '/oauth2/token',
-
-      //Development
-      tokenURL: 'https://apiit-internal.kdc.capitalone.com/oauth/oauth20/token',
-
-      //For Sandbox IT
-      //tokenURL: 'https://api-sandbox-external-it.cloud.capitalone.com/oauth2/token',//Internal
-      //tokenURL: 'https://api-sandbox.capitalone.com/oauth2/token',//External
-
-
+      tokenURL: config.BASE_URI + '/oauth2/token',
       // The clientId and clientSecret you received when registering your app.
       clientID: config.CLIENT_ID,
       clientSecret: config.CLIENT_SECRET
@@ -50,8 +38,7 @@ var ClientOptions =  {
     url: config.BASE_URI,
     apiVersion: 2
   };
-//var PORT = process.env.PORT || 8001;
-var PORT = process.env.PORT || 7001;
+var PORT = process.env.PORT || 8001;
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
